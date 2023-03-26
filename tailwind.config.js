4c50ad6134275f297b70cb2cf83443d7697cc2cb
valer-y <yazykov.valery@gmail.com>
2023-03-26 21:13:19 +0200

    adjust npm

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
